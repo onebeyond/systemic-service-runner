819788d50b7f5771064a994ec0051d3953714bce
Ulises Gascon <ulises.gascon@guidesmiths.com>
2019-12-13 10:19:05 +0100

    Bug fixed
    - Related #2

--- index.js
+++ index.js
@@ -14,6 +14,23 @@ module.exports = (system, options = {}) => {
         system.start((err, components) => {
             if (err) return cb(err)
 
+            const scheduleRestart = () => {
+                const delay = Math.ceil(Math.random() * duration(config.restart.window) / 1000) * 1000
+                logger.info(format('Service will restart in %s seconds.', delay / 1000))
+
+                clearTimeout(timeout)
+                timeout = setTimeout(() => {
+                    system.restart(err => {
+                        if (err) {
+                            logger.error('Error restarting system.')
+                            logger.error(err)
+                            process.exit(1)
+                        }
+                    })
+                }, delay)
+                timeout.unref()
+            }
+            
             process.on('systemic_restart', scheduleRestart)
 
             process.on('error', err => {
@@ -34,23 +51,6 @@ module.exports = (system, options = {}) => {
                     system.stop(() => process.exit(0))
                 })
             })
-
-            const scheduleRestart = () => {
-                const delay = Math.ceil(Math.random() * duration(config.restart.window) / 1000) * 1000
-                logger.info(format('Service will restart in %s seconds.', delay / 1000))
-
-                clearTimeout(timeout)
-                timeout = setTimeout(() => {
-                    system.restart(err => {
-                        if (err) {
-                            logger.error('Error restarting system.')
-                            logger.error(err)
-                            process.exit(1)
-                        }
-                    })
-                }, delay)
-                timeout.unref()
-            }
 
             cb(null, components)
         })
